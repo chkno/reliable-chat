ca612dcb40cb644930b74c84e55218abda34e255
Scott Worley <sworley@chkno.net>
2012-07-28 12:06:43 -0700

    Test fetch blocking
    Fetch blocks until message is sent.
    Fetches initiated before a speak return the later-spoken value.

--- server/server_test.go
+++ server/server_test.go
@@ -1,16 +1,43 @@
 package main
 
 import "testing"
+import "runtime"
 import "time"
 
 func TestMessageInsertAndRetreive(t *testing.T) {
-	say := ""
+	say := "'Ello, Mister Polly Parrot!"
 	at := time.Now()
 	var zero_time time.Time
 	store := start_store()
 	store.Add <- &Message{at, say}
 	messages_from_store := make(chan []Message, 1)
 	store.Get <- &StoreRequest{zero_time, messages_from_store}
+	messages := <-messages_from_store
+	if len(messages) != 1 {
+		t.Fail()
+	}
+	if messages[0].Time != at {
+		t.Fail()
+	}
+	if messages[0].Text != say {
+		t.Fail()
+	}
+	close(store.Get)
+	close(store.Add)
+}
+
+func TestFetchBlocksUntilSpeak(t *testing.T) {
+	start_fetch_wait_count := fetch_wait_count.String()
+	say := "I've got a lovely fresh cuttle fish for you"
+	at := time.Now()
+	var zero_time time.Time
+	store := start_store()
+	messages_from_store := make(chan []Message, 1)
+	store.Get <- &StoreRequest{zero_time, messages_from_store}
+	for start_fetch_wait_count == fetch_wait_count.String() {
+		runtime.Gosched()
+	}
+	store.Add <- &Message{at, say}
 	messages := <-messages_from_store
 	if len(messages) != 1 {
 		t.Fail()
